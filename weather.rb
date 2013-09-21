7ca49b965014a560eb7475f64be46adba48bddac
MakerSquare Student <students@themakersquare.com>
2013-09-21 14:56:08 -0500

    Use geolocation api to get a lat and long.

--- weather.rb
+++ weather.rb
@@ -6,22 +6,38 @@ require 'json'
 require 'rest-client'
 require 'forecast_io'
 require 'typhoeus/adapters/faraday'
+require 'geocoder'
+require 'foreman'
 
 Faraday.default_adapter = :typhoeus
-
+ForecastIO.api_key = ENV['KEY']
 
 get '/' do
   erb :index
 end
 
 post '/results' do
+  # forecast = ForecastIO.forecast(params[:latitude], params[:longitude])
+  # @gamble =  'http://maps.googleapis.com/maps/api/geocode/json?address=sydney&sensor=false'
+  url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{params[:city]}&sensor=false"
+  response = RestClient.get url, :accept => :json
+  response2 = JSON.load(response)
+  response3 = response2['results']
+  response4 = response3[0]
+  response5 = response4['geometry']
+  response6 = response5['location']
+  lat = response6['lat']
+  lng = response6['lng']
   forecast = ForecastIO.forecast(37.8267, -122.423)
-  @gamble = forecast.hourly.summary
+  @gamble =  forecast
   erb :results
 end
 
+# url = 'http://maps.googleapis.com/maps/api/geocode/json?address=sydney&sensor=false'
+# response = RestClient.get url, :accept => :json
+# response = JSON.load(response)
 
 
 
 
-ForecastIO.api_key = '47b3838c61368a05d35630e2a7b8bfd9'
+
