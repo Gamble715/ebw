0a1ff2c92b13fadb1f34c1155c8d1cb3e0495694
MakerSquare Student <students@themakersquare.com>
2013-09-21 17:13:07 -0500

    Add ability to see map.

--- weather.rb
+++ weather.rb
@@ -32,8 +32,21 @@ post '/results' do
   @daySummary = forecast.hourly.summary
   @dayPrecipitation = forecast.minutely.icon
   erb :results
-    
 end
+
+post '/map' do
+  url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{params[:city]}&sensor=false"
+  response = RestClient.get url, :accept => :json
+  response2 = JSON.load(response)
+  response3 = response2['results']
+  response4 = response3[0]
+  response5 = response4['geometry']
+  response6 = response5['location']
+  @lat = response6['lat']
+  @lng = response6['lng']
+  erb :map
+end
+
 
 
 
