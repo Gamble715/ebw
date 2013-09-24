abd766104c8d3c14e40c63f3ec80649b546b53c4
MakerSquare Student <students@themakersquare.com>
2013-09-24 10:51:40 -0500

    Add first version of geolocation.

--- weather.rb
+++ weather.rb
@@ -22,13 +22,19 @@ end
 
 post '/results' do
   # Convert string to URI ruby.. URL safe
-  url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{params[:city]}&sensor=false"
-  response = RestClient.get url, :accept => :json
-  response2 = JSON.load(response)
-  weather.lat = response2['results'][0]['geometry']['location']['lat']
-  weather.lng = response2['results'][0]['geometry']['location']['lng']
+  # url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{params[:city]}&sensor=false"
+  # response = RestClient.get url, :accept => :json
+  # response2 = JSON.load(response)
+
+  # weather.lat = response2['results'][0]['geometry']['location']['lat']
+  # weather.lng = response2['results'][0]['geometry']['location']['lng']
+
+  weather.lat = params['latitude']
+  weather.lng = params['longitude']
+
   @lat = weather.lat
   @lng = weather.lng
+
   forecast = ForecastIO.forecast(weather.lat, weather.lng)
   if forecast.minutely.nil?
       @error = "Please enter a valid zipcode."
@@ -37,7 +43,7 @@ post '/results' do
     weather.condition = forecast.minutely.icon
     weather.what_to_do
     # want abilitiy to change number of activities
-    @location_name = response2['results'][0]['address_components'][1]['short_name']
+    # @location_name = response2['results'][0]['address_components'][1]['short_name']
     @activities = weather.activity[0..5]
     @currentSummary = forecast.currently.summary
     @currentTemp = forecast.currently.temperature
